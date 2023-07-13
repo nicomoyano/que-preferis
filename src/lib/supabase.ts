58383dda2578b69e754ead142601b82d41c73d8a
Nicolas Moyano <nicomoyano01@gmail.com>
2023-07-13 19:27:39 -0300

    Hide supabase key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,8 @@
 import { Database } from '@/types/Database';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://rgbmwmciaabqnlzutwtc.supabase.co';
-const supabaseKey =
-  // 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJnYm13bWNpYWFicW5senV0d3RjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODkxMTk0MDMsImV4cCI6MjAwNDY5NTQwM30.dvd1ZObwSCrzKOvBq8W9JmyePQDCZreR9CmuD0cDMT0';
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJnYm13bWNpYWFicW5senV0d3RjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4OTExOTQwMywiZXhwIjoyMDA0Njk1NDAzfQ.RSvzGWH4xWMdbObzRX_I4Bw265znwRQzaVy6JrwvB4E';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY as string;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
   auth: {
